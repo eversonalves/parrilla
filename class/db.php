04b677d69cdab59bfa7e35204ca97502672915bd
EVERSON ALVES DOS REIS <everson.alves18@hotmail.com>
2025-10-14 09:59:34 -0300

    Igualando banco de dados (escola e casa).

--- class/db.php
+++ class/db.php
@@ -4,8 +4,8 @@
         static $pdo; 
         if ($pdo === null){    // ( = ) Atribuição ( == ) Comparação  ( === ) Comparação tipo e valor.
             $pdo = new PDO(
-                "mysql:host=192.168.20.113;dbname=tdszuphpdb01", // Server casa.
-                //"mysql:host=10.91.47.50;dbname=tdszuphpdb01",  // server escola.
+                //"mysql:host=192.168.20.113;dbname=tdszuphpdb01", // Server casa.
+                "mysql:host=10.91.47.50;dbname=tdszuphpdb01",  // server escola.
                 "root",  // Usuário do Banco de Dados.
                 "123",  //  Senha do Banco de Dados.
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
