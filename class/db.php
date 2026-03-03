88762eefaacd39997d647aa85e9e3b28db3e4c45
Everson Reis <everson.alves18@hotmail.com>
2026-03-03 11:03:29 -0300

    conecxao banco alterada.

--- class/db.php
+++ class/db.php
@@ -8,9 +8,9 @@
         if ($pdo === null){    // ( = ) Atribuição ( == ) Comparação  ( === ) Comparação tipo e valor.
             $pdo = new PDO(
                 //"mysql:host=192.168.20.113;dbname=tdszuphpdb01", // Server casa.
-                "mysql:host=10.91.47.222;dbname=tdszuphpdb01",  // server escola.
-                "root",  // Usuário do Banco de Dados.
-                "123",  //  Senha do Banco de Dados.
+                "mysql:host=10.53.160.6;dbname=evers710_reis_parrilla",  // server escola.
+                "evers710_reis",  // Usuário do Banco de Dados.
+                "@E88096293r",  //  Senha do Banco de Dados.
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
                 ]
