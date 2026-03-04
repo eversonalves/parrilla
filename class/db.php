510ba4da2c4f4cdd6dc94ab3158a56a1ea53cdf4
Everson Alves dos Reis <everson.alves18@hotmail.com>
2026-03-04 10:01:24 -0300

    att de conexao com o banco

--- class/db.php
+++ class/db.php
@@ -1,16 +1,11 @@
-<?php 
-
-    namespace App;
-    use PDO;
-
-    function getConnection(): PDO{  //  Retorna um objeto PDO.
+<?php    // Conexão com o Banco de dados
+    function getConnection(): PDO{ 
         static $pdo; 
         if ($pdo === null){    // ( = ) Atribuição ( == ) Comparação  ( === ) Comparação tipo e valor.
             $pdo = new PDO(
-                //"mysql:host=192.168.20.113;dbname=tdszuphpdb01", // Server casa.
-                "mysql:host=10.53.160.6;dbname=evers710_reis_parrilla",  // server escola.
-                "evers710_reis",  // Usuário do Banco de Dados.
-                "@E88096293r",  //  Senha do Banco de Dados.
+                "mysql:host=10.53.160.6;dbname=evers710_reis_parrilla", // ip do banco e nome do banco
+                "evers710_reis",  // usuario do banco
+                "@E88096293r",  // senha do usuario do banco
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
                 ]
